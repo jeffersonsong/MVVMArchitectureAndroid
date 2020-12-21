5931183312102ef488555cd4729d5bf32085f00c
Jefferson Song <jeffersonsong@yahoo.com>
2020-12-20 21:00:07 -0500

    Data binding.

--- app/src/main/java/com/mindorks/framework/mvvm/ui/main/view/MainActivity.kt
+++ app/src/main/java/com/mindorks/framework/mvvm/ui/main/view/MainActivity.kt
@@ -4,12 +4,15 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.mindorks.framework.mvvm.R
 import com.mindorks.framework.mvvm.data.model.User
+import com.mindorks.framework.mvvm.databinding.ActivityMainBinding
 import com.mindorks.framework.mvvm.ui.base.ViewModelFactory
 import com.mindorks.framework.mvvm.ui.main.adapter.MainAdapter
 import com.mindorks.framework.mvvm.ui.main.viewmodel.MainViewModel
@@ -26,16 +29,18 @@ class MainActivity : AppCompatActivity() {
     lateinit var viewModelFactory: ViewModelFactory
     private lateinit var mainViewModel: MainViewModel
     private lateinit var adapter: MainAdapter
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         setupUI()
         setupViewModel()
         setupObserver()
     }
 
     private fun setupUI() {
+        val recyclerView: RecyclerView = binding.recyclerView
         adapter = MainAdapter(arrayListOf())
         bindRecyclerViewAdapter(recyclerView, adapter)
     }
